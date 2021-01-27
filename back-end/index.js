aea961e877550551641702e8c6b66189ab452dfb
Miguel González Aravena <miguel.gonzalez.93@gmail.com>
2021-01-27 03:46:24 -0300

    Agregar whitelist

--- back-end/index.js
+++ back-end/index.js
@@ -9,7 +9,37 @@ const indicators = require('./private/routes/indicators.router');
 
 app
   .use(bodyParser.urlencoded({ extended: false }))
-  .use(bodyParser.json())
+  .use(bodyParser.json());
+
+// Whitelist server and enable CORS
+// TO-DO: Add config with all whitelisted servers
+var whitelist = [
+  '127.0.0.1'
+];
+
+app.use((req, res, next) => {
+  let ip = req.headers['x-real-ip'] !== undefined ? req.headers['x-real-ip'] : '127.0.0.1';
+
+  let isLocal = ip == '127.0.0.1' || req.headers.origin === undefined;
+  let canReply = whitelist.indexOf(ip) !== -1;
+  let origin = canReply ? (!isLocal ? req.headers.origin : '*') : '';
+  
+  if (canReply) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
+    res.setHeader('Access-Control-Max-Age', '60');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    
+    return next();
+  } else {
+    return res.json(reply.error('This is CORS-enabled for a whitelisted domain.'));
+  }
+});
+  
 
 app.get('/', (req, res) => { res.json({ status: 'OK' }) });
 
